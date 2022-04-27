520f843b6ff6b2cf57143123af08d0c0d0f06920
Rahul Yadav <rahulkyadav9691@gmail.com>
2022-04-28 01:20:26 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import Axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [friendsList, setFriendsList] = useState([]);
+  const addFriend = () => {
+    Axios.post('http://localhost:3001/addFriend',
+      {
+        name: name,
+        age: age
+      })
+  }
+  const deleteFriend = (id) => {
+      Axios.delete(`http://localhost:3001/delete/${id}`)
+  }
+  useEffect(() => {
+    Axios.get('http://localhost:3001/read').then(resp => {
+      setFriendsList(resp.data);
+    })
+  }, [friendsList])
+
+  const updateFriend = (id) => {
+    const newAge = prompt('enter the new age');
+    Axios.put('http://localhost:3001/update', { newAge: newAge, id: id });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="inputs">
+        <input type="text"
+          placeholder="enter friends name ..."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="number"
+          placeholder="enter friends age..."
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button onClick={addFriend}>Add friend</button>
+      </div>
+      <div className="friends-container">
+        {friendsList.map(ele => {
+          return (
+            <div className="friendcontainer">
+              <div className="friend">
+                <h3>Name:{ele.name}</h3>
+                <h3>age:{ele.age}</h3>
+              </div>
+              <button onClick={() => updateFriend(ele._id)}>update</button>
+              <button onClick = {()=>deleteFriend(ele._id)}>delete</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
